bd1f37295dfc48e4a394e1c558a7f6c01172bdcb
gdrakoulakos <georgediopan@gmail.com>
2023-12-25 23:37:49 +0200

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,112 @@
-import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { ApolloClient, InMemoryCache, createHttpLink, gql } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Select, MenuItem } from "@mui/material";
 import styles from '../styles/Home.module.css';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 export default function Home() {
+
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query {
+            repository(owner: "reactjs", name: "reactjs.org") {
+              issues (first: 10, orderBy: { field: CREATED_AT, direction: DESC }) {
+                edges {
+                  node {
+                    title
+                    body
+                    createdAt
+                    state
+                  }
+                }
+              }
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        console.log(result?.data);
+        setIssues(result?.data?.repository?.issues?.edges)
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
+      <header>
+        <title>BugsBuddy</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </header>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section>
+          <h1 className={styles.title}>
+            Welcome to <br/>
+            <img className={styles.bugsBuddyLogo} src='../images/logo.jpg' alt='BugsBuddy logo'></img>
+          </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <p className={styles.description}>
+            Get started by selecting the following filter
+          </p>
+        </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <section>
+          <Select
+            sx={{        
+              width: 250,
+              height: 50,
+            }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <MenuItem value={1}>All</MenuItem>
+            <MenuItem value={2}>Open</MenuItem>
+            <MenuItem value={3}>Closed</MenuItem>
+          </Select>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          {issues?.map((issue)=> (
+            <div>
+              <h2>{issue.node.title} </h2>
+              <p className={styles.issueBody}>{issue.node.body}</p>
+              <p>{new Date(issue.node.createdAt).toLocaleString()}</p>
+              <p>{issue.node.state}</p>
+            </div>
+          ))}
+          
+        
+          
+
+        </section>
+
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+      © {new Date().getFullYear()} George Drakoulakos
       </footer>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
